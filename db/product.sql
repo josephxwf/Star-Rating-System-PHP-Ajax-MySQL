b4455238d7ae6a769245dc27d59fc9f17f2dced1
Owner <josephxwf@gmail.com>
2019-03-14 17:26:37 -0400

    fix database sacheme

--- db/product.sql
+++ db/product.sql
@@ -12,4 +12,4 @@ CREATE TABLE `product` (
 
 INSERT INTO `product` (`id`, `name`, `description`, `price`, `image`, `created_at`, `updated_at`) VALUES
 (1, 'Frog Toy', '<ul style="margin:0px;padding:15px;">\r\n					<li>Built-in voice box </li>\r\n					<li>soft and great for cuddling</li>\r\n				<li>Special Design</li>\r\n			</ul>	', 12, 'frog.png', '2019-01-19 14:13:04', '2019-01-19 14:13:04'),
-(2, 'Chicken Toy', '<ul style="margin:0px;padding:15px;">\r\n				<li>Built-in voice box </li>\r\n					<li>soft and great for cuddling</li>\r\n				<li>Special Design</li>\r\n			</ul>	', 21, 'chicken.jpg', '2019-01-19 14:13:04', '2019-01-19 14:13:04');
+(2, 'Chicken Toy', '<ul style="margin:0px;padding:15px;">\r\n				<li>Built-in voice box </li>\r\n					<li>soft and great for cuddling</li>\r\n				<li>Special Design</li>\r\n			</ul>	', 21, 'chicken.png', '2019-01-19 14:13:04', '2019-01-19 14:13:04');
--- db/product_rating.sql
+++ db/product_rating.sql
@@ -2,37 +2,40 @@
 -- Table structure for table `product_rating`
 --
 
+
+
 CREATE TABLE `product_rating` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `product_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
   `rating_score` int(11) NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `comments` text COLLATE utf8_unicode_ci NOT NULL,
+  `comment` text COLLATE utf8_unicode_ci,
+  `reviewer` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 = Block, 0 = Unblock',
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1 = Block, 0 = Unblock'
+  `updated_at` datetime NOT NULL
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 
 --
 -- Dumping data for table `product_rating`
 --
 
-INSERT INTO `product_rating` (`id`, `product_id`, `user_id`, `rating_score`, `title`, `comments`, `created_at`, `updated_at`, `status`) VALUES
-(1, 1, 1, 4, 'its awesome', 'It''s awesome!!!', '2018-08-19 09:13:01', '2018-08-19 09:13:01', 1),
-(2, 2, 2, 5, 'Nice product', 'Really quality product!', '2018-08-19 09:13:37', '2018-08-19 09:13:37', 1),
-(3, 3, 3, 3, 'I like it', 'its''s best but item.', '2018-08-19 09:14:19', '2018-08-19 09:14:19', 1),
-(4, 4, 4, 3, 'super awesome ', 'i think its supper products', '2018-08-19 09:18:00', '2018-08-19 09:18:00', 1),
-(5, 5, 5, 1, 'Good idea', 'Nice', '2019-01-20 17:00:58', '2019-01-20 17:00:58', 1),
-(6, 6, 5, 5, 'Nice product', 'this is nice!', '2019-01-20 17:01:37', '2019-01-20 17:01:37', 1),
-(7, 7, 3, 4, 'really nice', 'Good!', '2019-01-20 21:06:48', '2019-01-20 21:06:48', 1);
+INSERT INTO `product_rating` (`id`, `product_id`, `user_id`, `rating_score`, `title`, `comment`, `reviewer`, `email`, `status`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 4, 'its awesome', 'It''s awesome!!!','Joseph', 'joseph123@gmail.com',1, '2018-08-19 09:13:01', '2018-08-19 09:13:01'),
+(2, 2, 2, 5, 'Nice product', 'Really quality product!','Mary', 'mary123@gmail.com',1,  '2018-08-19 09:13:37', '2018-08-19 09:13:37'),
+(3, 1, 3, 3, 'I like it', 'its''s best but item.', 'John', 'john123@gmail.com', 1,'2018-08-19 09:14:19', '2018-08-19 09:14:19'),
+(4, 1, 4, 3, 'super awesome ', 'i think its supper products','lisa', 'lisa123@gmail.com', 1, '2018-08-19 09:18:00', '2018-08-19 09:18:00'),
+(5, 2, 5, 1, 'Good idea', 'Nice', 'Daniel', 'Daniel123@gmail.com',1, '2019-01-20 17:00:58',  '2019-01-20 17:00:58'),
+(6, 2, 5, 5, 'Nice product', 'this is nice!', 'Lee', 'Lee23@gmail.com',1, '2019-01-20 17:01:37', '2019-01-20 17:01:37'),
+(7, 1, 3, 4, 'really nice', 'Good!', 'Michael', 'Michael23@gmail.com', 1,'2019-01-20 21:06:48', '2019-01-20 21:06:48');
 
 
 
 
-ALTER TABLE `product_rating` ADD `reviewer` VARCHAR(255) NULL DEFAULT NULL AFTER `comments`;
 
-ALTER TABLE `product_rating` ADD `email` VARCHAR(255) NOT NULL AFTER `reviewer`;
 
 ALTER TABLE `product_rating` CHANGE `user_id` `user_id` INT(11) NULL DEFAULT NULL;
